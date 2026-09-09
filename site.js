a564248374e8c3ac5c3432b1621b33206c33ade7
BP <brady.pennings9388@fvtc.edu>
2026-09-09 13:32:46 -0500

    Wk3Work

--- site.js
+++ site.js
@@ -1,1 +1,17 @@
 console.log("site.js linked successfully");
+
+const hours = new Date().getHours();
+
+const isMorning = hours >= 4 && hours < 12;
+const isAfternoon = hours >= 12 && hours < 17;
+const isEvening = hours >= 17 || hours < 4;
+
+const welcome = document.querySelector("#welcome");
+
+if (isMorning) {
+	welcome.textContent = "Good Morning!";
+} else if (isAfternoon) {
+	welcome.textContent = "Good Afternoon!";
+} else if (isEvening) {
+	welcome.textContent = "Good Night!";
+}
